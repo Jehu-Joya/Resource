c0258e690f5a0879baf9579e5450c28d72689333
Jehu <Jehu.Joya@gmail.com>
2023-01-11 09:12:42 +0800

    Init

---
+++ fq/loon/proxy/proxy-null.sh
@@ -0,0 +1,3 @@
+time=$(date '+%Y.%m.%d %H:%M:%S')
+
+echo "$time 无节点 = vmess,apple.com,80,chacha20-ietf-poly1305,"86155150-d86d-42e2-af55-986e3d72323a",over-tls:false,transport:ws,path:/Patti,host:apple.com,skip-cert-verify:1"
